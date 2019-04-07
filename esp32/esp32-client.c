9ae1107892037fc6eeff846cc993ae933aff47e8
Archit Kapoor <archiekpr@gmail.com>
2019-04-07 14:40:33 +0530

    added files to github repo

---
+++ esp32/esp32-client.c
@@ -0,0 +1,193 @@
+
+#include <string.h>
+#include <sys/param.h>
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/event_groups.h"
+#include "esp_system.h"
+#include "esp_wifi.h"
+#include "esp_event_loop.h"
+#include "esp_log.h"
+#include "nvs_flash.h"
+
+#include "lwip/err.h"
+#include "lwip/sockets.h"
+#include "lwip/sys.h"
+#include <lwip/netdb.h>
+
+
+#define WIFI_SSID CONFIG_WIFI_SSID
+#define WIFI_PASSWORD CONFIG_WIFI_PASSWORD
+
+#ifdef CONFIG_EXAMPLE_IPV4
+#define SERVER_IP_ADDR CONFIG_EXAMPLE_IPV4_ADDR
+#else
+#define SERVER_IP_ADDR CONFIG_EXAMPLE_IPV6_ADDR
+#endif
+
+#define PORT CONFIG_EXAMPLE_PORT
+
+/* FreeRTOS event group to signal when we are connected & ready to make a request */
+static EventGroupHandle_t wifi_event_group;
+
+const int IPV4_GOTIP_BIT = BIT0;
+const int IPV6_GOTIP_BIT = BIT1;
+
+static const char *TAG = "intruder_detection_system";
+static char data_to_send[128];
+
+static esp_err_t event_handler(void *ctx, system_event_t *event)
+{
+    switch (event->event_id) {
+    case SYSTEM_EVENT_STA_START:
+        esp_wifi_connect();
+        ESP_LOGI(TAG, "SYSTEM_EVENT_STA_START");
+        break;
+    case SYSTEM_EVENT_STA_CONNECTED:
+        /* enable ipv6 */
+        tcpip_adapter_create_ip6_linklocal(TCPIP_ADAPTER_IF_STA);
+        break;
+    case SYSTEM_EVENT_STA_GOT_IP:
+        xEventGroupSetBits(wifi_event_group, IPV4_GOTIP_BIT);
+        ESP_LOGI(TAG, "SYSTEM_EVENT_STA_GOT_IP");
+        break;
+    case SYSTEM_EVENT_STA_DISCONNECTED:
+        /* This is a workaround as ESP32 WiFi libs don't currently auto-reassociate. */
+        esp_wifi_connect();
+        xEventGroupClearBits(wifi_event_group, IPV4_GOTIP_BIT);
+        xEventGroupClearBits(wifi_event_group, IPV6_GOTIP_BIT);
+        break;
+    case SYSTEM_EVENT_AP_STA_GOT_IP6:
+        xEventGroupSetBits(wifi_event_group, IPV6_GOTIP_BIT);
+        ESP_LOGI(TAG, "SYSTEM_EVENT_STA_GOT_IP6");
+
+        char *ip6 = ip6addr_ntoa(&event->event_info.got_ip6.ip6_info.ip);
+        ESP_LOGI(TAG, "IPv6: %s", ip6);
+    default:
+        break;
+    }
+    return ESP_OK;
+}
+
+static void initialise_wifi(void)
+{
+    tcpip_adapter_init();
+    wifi_event_group = xEventGroupCreate();
+    ESP_ERROR_CHECK( esp_event_loop_init(event_handler, NULL) );
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK( esp_wifi_init(&cfg) );
+    ESP_ERROR_CHECK( esp_wifi_set_storage(WIFI_STORAGE_RAM) );
+    wifi_config_t wifi_config = {
+        .sta = {
+            .ssid = WIFI_SSID,
+            .password = WIFI_PASSWORD,
+        },
+    };
+    ESP_LOGI(TAG, "Setting WiFi configuration SSID %s...", wifi_config.sta.ssid);
+    ESP_ERROR_CHECK( esp_wifi_set_mode(WIFI_MODE_STA) );
+    ESP_ERROR_CHECK( esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_config) );
+    ESP_ERROR_CHECK( esp_wifi_start() );
+}
+
+static void wait_for_ip()
+{
+    uint32_t bits = IPV4_GOTIP_BIT | IPV6_GOTIP_BIT ;
+
+    ESP_LOGI(TAG, "Waiting for AP connection...");
+    xEventGroupWaitBits(wifi_event_group, bits, false, true, portMAX_DELAY);
+    ESP_LOGI(TAG, "Connected to AP");
+}
+
+static void tcp_client_task(void *pvParameters)
+{
+    char rx_buffer[128];
+    char addr_str[128];
+    int addr_family;
+    int ip_protocol;
+
+    while (1) {
+
+#ifdef CONFIG_EXAMPLE_IPV4
+        struct sockaddr_in destAddr;
+        destAddr.sin_addr.s_addr = inet_addr(SERVER_IP_ADDR);
+        destAddr.sin_family = AF_INET;
+        destAddr.sin_port = htons(PORT);
+        addr_family = AF_INET;
+        ip_protocol = IPPROTO_IP;
+        inet_ntoa_r(destAddr.sin_addr, addr_str, sizeof(addr_str) - 1);
+#else // IPV6
+        struct sockaddr_in6 destAddr;
+        inet6_aton(SERVER_IP_ADDR, &destAddr.sin6_addr);
+        destAddr.sin6_family = AF_INET6;
+        destAddr.sin6_port = htons(PORT);
+        addr_family = AF_INET6;
+        ip_protocol = IPPROTO_IPV6;
+        inet6_ntoa_r(destAddr.sin6_addr, addr_str, sizeof(addr_str) - 1);
+#endif
+
+        int sock =  socket(addr_family, SOCK_STREAM, ip_protocol);
+        if (sock < 0) {
+            ESP_LOGE(TAG, "Unable to create socket: errno %d", errno);
+            break;
+        }
+        ESP_LOGI(TAG, "Socket created");
+
+        int err = connect(sock, (struct sockaddr *)&destAddr, sizeof(destAddr));
+        if (err != 0) {
+            ESP_LOGE(TAG, "Socket unable to connect: errno %d", errno);
+            break;
+        }
+        ESP_LOGI(TAG, "Successfully connected");
+
+        while (1) {
+
+            // Store information related to AP
+            wifi_ap_record_t ap_info;
+            ESP_ERROR_CHECK( esp_wifi_sta_get_ap_info(&ap_info));
+            ESP_LOGI(TAG, "AP SSID is: %s", ap_info.ssid);
+            ESP_LOGI(TAG, "AP MAC Address is: %s", ap_info.bssid);
+            ESP_LOGI(TAG, "AP RSSI is: %hd", ap_info.rssi);
+            // data_to_send = ap_info.rssi;
+            sprintf(data_to_send, "%hd", ap_info.rssi);
+
+            // Send data to server
+            int err = send(sock, data_to_send, sizeof(data_to_send), 0);
+            if (err < 0) {
+                ESP_LOGE(TAG, "Error occured during sending: errno %d", errno);
+                break;
+            }
+
+            int len = recv(sock, rx_buffer, sizeof(rx_buffer) - 1, 0);
+            // Error occured during receiving
+            if (len < 0) {
+                ESP_LOGE(TAG, "recv failed: errno %d", errno);
+                break;
+            }
+            // Data received
+            else {
+                rx_buffer[len] = 0; // Null-terminate whatever we received and treat like a string
+                ESP_LOGI(TAG, "Received %d bytes from %s:", len, addr_str);
+                ESP_LOGI(TAG, "%s", rx_buffer);
+            }
+
+            vTaskDelay(50);
+        }
+
+        if (sock != -1) {
+            ESP_LOGE(TAG, "Shutting down socket and restarting...");
+            shutdown(sock, 0);
+            close(sock);
+        }
+    }
+    vTaskDelete(NULL);
+}
+
+void app_main()
+{
+    ESP_ERROR_CHECK( nvs_flash_init() );
+    initialise_wifi();
+    wait_for_ip();
+
+    xTaskCreate(tcp_client_task, "tcp_client", 4096, NULL, 5, NULL);
+}
---
+++ raspberry-pi/raspi-server.py
@@ -0,0 +1,87 @@
+import socket
+import threading
+import copy
+
+# Lists to store data received by ESP32s
+# Declare more lists if more ESP32s used
+esp32a_data_list = []
+esp32b_data_list = []
+
+
+def runServer(server_ip, server_port, server_no):
+    """
+    Start a server to receive data from the ESP32 modules
+    :param server_ip: IP address of Raspberry PI
+    :param server_port: Port number to which the ESP32 must connect
+    :param server_no: Identifier for which ESP32 is connected
+    """
+
+    # Create a socket at given IP and port and make the server bind to it
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    address = socket.getaddrinfo(server_ip, server_port)
+    server.bind(address[0][4])
+    server.listen(5)
+
+    print("\nServer Started at {}:{}\n".format(server_ip, server_port))
+
+    conn, addr = server.accept()
+    print("\nConnection Established at {}:{}\n".format(server_ip, server_port))
+
+    while True:
+        rssi_value = (conn.recv(1024)).decode()
+
+        # Data received is a string of the form - '-25\x00\x00\...'
+        # Split data to store only RSSI information
+        if server_no is 1:
+            esp32a_data_list.append(int(rssi_value.rstrip('\x00')))
+            # print(esp32a_data_list)
+        else:
+            esp32b_data_list.append(rssi_value)
+
+        # Reply acknowledgement to ESP32
+        conn.send(b'1')
+
+    # No more data left to send
+    print("\nClosing Connection at {}:{}\n".format(server_ip, server_port))
+    conn.close()
+
+    server.close()
+    print("\nServer Closed at {}:{}\n".format(server_ip, server_port))
+
+
+def setup():
+
+    server_ip = input("Enter my IP address: ")
+    server_port_1 = int(input("Enter port number for server A: "))
+    server_port_2 = int(input("Enter port number for server B: "))
+
+    # Start the servers
+    server_A = threading.Thread(target=runServer, args=(server_ip, server_port_1, 1, ))
+    server_A.start()
+
+    server_B = threading.Thread(target=runServer, args=(server_ip, server_port_2, 2, ))
+    server_B.start()
+
+    while True:
+        if len(esp32a_data_list) == 30:
+            data_list = copy.deepcopy(esp32a_data_list)
+            sig_prog_A = threading.Thread(target=displayList, args=(data_list, 1, ))
+            sig_prog_A.start()
+            esp32a_data_list.clear()
+        if len(esp32b_data_list) == 30:
+            data_list = copy.deepcopy(esp32b_data_list)
+            sig_prog_B = threading.Thread(target=displayList, args=(data_list, 2, ))
+            sig_prog_B.start()
+            esp32b_data_list.clear()
+
+
+def displayList(data_list, server_no):
+    """
+    Signal Processing Function
+    """
+    print("\nSignal Processing Function begins here\n")
+    print("Data received from {}: {}".format(server_no, data_list))
+
+
+if __name__ == "__main__":
+    setup()
