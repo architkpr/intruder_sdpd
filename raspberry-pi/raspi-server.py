7eacfb71233690e1b92483613a72dba508af9d75
Archit Kapoor <archiekpr@gmail.com>
2019-04-09 01:59:05 +0530

    minor fixes

--- raspberry-pi/raspi-server.py
+++ raspberry-pi/raspi-server.py
@@ -191,7 +191,7 @@ def displayList(data_list, server_no, intruder_firebase):
 
         # intruder_firebase.put('timelog', 'time for ' + str(count), 'intruder detected on ESP : ' + str(server_no) + 'at time ' + current_time_1)
 
-        intruder_firebase.put('timelog', 'Entry: ' + str(count), 'At ' + current_time_hour + ':' + current_time_min + ':' + current_time_sec + ' INTRUDER DETECTED AT ESP : ' + str(server_no))
+        intruder_firebase.put('timelog', 'Entry: ' + str(count), 'AT ' + current_time_hour + ':' + current_time_min + ':' + current_time_sec + ' INTRUDER DETECTED AT ESP : ' + str(server_no))
 
     # plt.plot(data_list)
     # plt.plot(variance1, color='red')
